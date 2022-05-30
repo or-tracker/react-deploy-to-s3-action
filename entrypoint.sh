adff2ad07647dd5ee2bcb74328c40046d028033d
Arman Sharafshahi <asharafshahi@gmail.com>
2022-05-30 11:24:33 -0400

    bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,7 +43,7 @@ EOF
 # - Build react bundle
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
-sh -c "cd ${WORKING_DIRECTORY}"
+sh -c "cd ${WORKING_DIRECTORY}" \
 && sh -c "yarn" \
 && sh -c "${NODE_ENV_PREPEND} yarn build" \
 && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
